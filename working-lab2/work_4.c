c421a3aabfc4935b1c79494e5630a83ea2f12d16
PashchenkoYaroslav <59960114+PashchenkoYaroslav@users.noreply.github.com>
2021-02-08 18:38:15 +0300

    Update work_4.c

--- working-lab2/work_4.c
+++ working-lab2/work_4.c
@@ -2,14 +2,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char *argv[], char **envp)
+int main (int argc, char *argv[], char *envp[])
 {
-pid_t ppid, pid;
 int this_pid = getpid();
 fork();
 if(getppid() == this_pid)
 {
-execle("/bin/hw","hw",0,envp);
+execle("/bin/date","date",0,envp);
 printf("An error occured!");
 exit(-1);
 }
