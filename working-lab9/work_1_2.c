81430d1fc23aa51995b6a5050cf4058a85671fc9
user <yavpaschenko_1@edu.hse.ru>
2021-04-12 14:20:12 +0300

    Lab 9 done.

---
+++ working-lab9/work_1_2.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+
+int main()
+{
+
+    int semid; 
+
+    char pathname[] = "work_1_1.c"; 
+
+    key_t key; 
+
+    struct sembuf mybuf; 
+
+    if((key = ftok(pathname,0)) < 0){
+        printf("Can\'t generate key\n");
+        return(-1);
+    } 
+
+    if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0){
+        printf("Can\'t get semid\n");
+        return(-1);
+    } 
+
+    mybuf.sem_op = 1;
+    mybuf.sem_flg = 0;
+    mybuf.sem_num = 0;
+
+    if(semop(semid, &mybuf, 1) < 0){
+        printf("Can\'t wait for condition\n");
+        return(-1);
+    }
+
+    printf("Set condition\n");
+    return 0;
+}
