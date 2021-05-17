9c4d708e6f44e7a2c46fafee969d2191a60a0df6
user <yavpaschenko_1@edu.hse.ru>
2021-05-17 16:22:37 +0300

    Lab 11 task 3 with extra 2 donw

---
+++ working-lab11/work_3_server.c
@@ -0,0 +1,67 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+    int msqid;
+    char pathname[] = "work_3_server.c";
+    key_t key;
+    int msgLen, msgMaxLen;
+
+    struct requestBuf {
+        long mtype;
+        struct {
+            pid_t pid;
+            double message;
+        } info;
+    } clientBuf;
+
+    struct responseBuf {
+        long mtype;
+        struct {
+            double message;
+        } info;
+    } serverBuf;
+
+    if ((key = ftok(pathname, 0)) < 0) {
+        printf("Failed to generate key\n");
+        return(-1);
+    }
+    if ((msqid = msgget(key, 0666 | IPC_CREAT)) < 0) {
+        printf("Failed to get msqid\n");
+        return(-1);
+    }
+    printf("Server has started succesfully\n");
+    while (true) {
+        msgMaxLen = sizeof(clientBuf.info);
+        if (msgrcv(msqid, (struct clientmsgbuf *) &clientBuf, msgMaxLen, -2, 0) < 0) {
+            printf("Failed to receive message from queue\n");
+            return(-1);
+        }
+	if(clientBuf.mtype ==2){
+            printf("This server was killed!\nShutting down...\n");
+	return(-1);
+	}
+        printf("Received data: Client PID = %d, Data = %lf\n", clientBuf.info.pid, clientBuf.info.message);
+        double squareMes = clientBuf.info.message * clientBuf.info.message;
+        printf("The response are %lf\n", squareMes);
+
+        serverBuf.mtype = clientBuf.info.pid;
+        serverBuf.info.message = squareMes;
+        msgLen = sizeof(serverBuf.info);
+        if (msgsnd(msqid, (struct sermsgbuf *) &serverBuf, msgLen, 0) < 0) {
+            printf("Failed to send message to queue\n");
+            msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
+            return(-1);
+        }
+        printf("Response sent succesfully\n");
+    }
+    return 0;
+}
