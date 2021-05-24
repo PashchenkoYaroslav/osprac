03fc08ed70d9e92cd7f02990a991fcbc1202149b
user <yavpaschenko_1@edu.hse.ru>
2021-05-24 23:42:11 +0300

    Find right solution at exactly 23:41

--- working-lab13/work_1.c
+++ working-lab13/work_1.c
@@ -1,45 +1,65 @@
+#include <sys/types.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
-#include <string.h>
-#include <malloc.h>
 
-void get_links(const char *const path, int path_len, int *const rdepth) {
-    char *new_path = (char *) malloc((path_len + 2) * sizeof(char));
-    strcpy(new_path, path);
-    if (new_path[path_len - 1] == 'z' || new_path[path_len - 1] == 'Z') {
-        strcat(new_path, "a");
-        path_len++;
-    } else {
-        new_path[path_len - 1]++;
-    }
-    if (symlink(path, new_path) == -1) {
-        *rdepth = 0;
-        return;
-    }
-    get_links(new_path, path_len, rdepth);
-    (*rdepth)++;
-    unlink(new_path);
-    free(new_path);
+#define BUFFER_NAME_SIZE 32
+
+void get_max_depth(int recursive_depth) {
+	char previous_file_name[BUFFER_NAME_SIZE];
+	char current_file_name[BUFFER_NAME_SIZE];
+
+	sprintf(previous_file_name, "test_number_%d", recursive_depth);
+	sprintf(current_file_name, "test_number_%d", recursive_depth + 1);
+
+	if (symlink(previous_file_name, current_file_name) != 0) {
+		printf("Error while creating symlink.\n");
+		exit(-4);
+	}
+	++recursive_depth;
+	int file_descriptor;
+	if ((file_descriptor = open(current_file_name, O_RDWR | O_CREAT, 0666)) < 0) {
+		printf("Calculation is over.\n");
+		printf("Max possible depth is %d\n", recursive_depth - 1);
+		if (remove(current_file_name) < 0) {
+			printf("Error while removing a link.\n");
+			exit(-5);
+		}
+		return;
+	}
+	if (close(file_descriptor) < 0) {
+		printf("Error while closing the file.\n");
+		exit(-3);
+	}
+
+	get_max_depth(recursive_depth);
+	if (remove(current_file_name) < 0) {
+		printf("Error while removing a link.\n");
+		exit(-5);
+	}
 }
 
 int main() {
-    FILE *f;
-    int rdepth;
-    printf("Calculating recursion depth...\n");
-    if ((f = fopen("a", "w")) == NULL) {
-        printf("Error occured while creating original file.");
-        exit(-1);
-    }
-    if (fputs("Original file", f) == EOF) {
-        printf("Error occured while writing to original file.");
-        exit(-1);
-    }
-    get_links("a", 1, &rdepth);
-    printf("Recursion depth is %d\n", rdepth);
-    if (fclose(f) == EOF) {
-        printf("Error occured while closing original file.");
-        exit(-1);
-    }
-    return 0;
+	char file_name[] = "test_number_0";
+	int file_descriptor;
+
+	if ((file_descriptor = open(file_name, O_RDWR | O_CREAT, 0666)) < 0) {
+		printf("Error while opening the file.\n");
+		exit(-1);
+	}
+
+	if (close(file_descriptor) < 0) {
+		printf("Error while closing the file.\n");
+		exit(-2);
+	}
+        printf("Calculation is starting...\n");
+	get_max_depth(0);
+	if (remove(file_name) < 0) {
+		printf("Error while removing the initial file.\n");
+		exit(-6);
+	}
+
+	return 0;
 }
