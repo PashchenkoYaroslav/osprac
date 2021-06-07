025ed71eb99af3aec2c111744c2ebdca42dca748
user <yavpaschenko_1@edu.hse.ru>
2021-06-07 16:44:59 +0300

    Fix last task

--- working-lab14/work_4_1.c
+++ working-lab14/work_4_1.c
@@ -1,41 +1,43 @@
-#include <sys/types.h>
-#include <signal.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
 
+int sender_pid;
+int index = 0;
 int value;
-int mask;
+bool received = false;
 
-void set_zero(int nsig) {
-  mask <<= 1;
+void sigusr1_handler_bit(int nsig) {
+	value |= (1 << index++);
+	kill(sender_pid, SIGUSR1);
 }
 
-void set_one(int nsig) {
-  value |= mask;
-  mask <<= 1;
+void sigusr2_handler_nobit(int nsig) {
+	index++;
+	kill(sender_pid, SIGUSR1);
+}
+
+void sigchld_handler(int nsig) {
+	received = true;
 }
 
 int main(void) {
-  pid_t sender_pid;
-  int   i;
-
-  value = 0;
-  mask  = 1;
-  (void) signal(SIGUSR1, set_zero);
-  (void) signal(SIGUSR2, set_one);
-
   printf("Receiver PID is %d\n", getpid());
 
   printf("Enter sender's PID : ");
   scanf("%d", &sender_pid);
 
-  while (1) {
-    if(mask == 0) {
-      printf("This program received a message : %d\n", value);
-      break;
-    }
-  }
+  signal(SIGUSR1, sigusr1_handler_bit);
+  signal(SIGUSR2, sigusr2_handler_nobit);
+  signal(SIGCHLD, sigchld_handler);
 
+  while (!received);
+
+  printf("This program received a message : %d\n", value);
   printf("Receiver has successfully completed its task\n");
   return 0;
 }
+
--- working-lab14/work_4_2.c
+++ working-lab14/work_4_2.c
@@ -1,32 +1,49 @@
-#include <sys/types.h>
-#include <signal.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+
+int receiver_pid;
+int value;
+bool can_send = true;
+
+void receiver_signal_handler(int nsig) {
+	can_send = true;
+}
+
+void send_value() {
+	int bits_count = sizeof(int) * 8;
+	for (int i = 0; i < bits_count; ++i) {
+		while (!can_send);
+		if ((value & (1 << i)) != 0) {
+			kill(receiver_pid, SIGUSR1);
+		}
+		else {
+			kill(receiver_pid, SIGUSR2);
+		}
+		can_send = false;
+	}
+	kill(receiver_pid, SIGCHLD);
+}
 
 int main(void) {
-  pid_t receiver_pid;
-  int   input, mask;
-  int   i;
-
+  signal(SIGUSR1, receiver_signal_handler);
   printf("Sender PID is %d\n", getpid());
 
   printf("Enter receiver's PID : ");
-  scanf("%d", &receiver_pid);
-
-  printf("Enter a message that is an integer: ");
-  scanf("%d", &input);
-
-  mask = 1;
-  while (mask != 0) {
-    if (input & mask) {
-      kill(receiver_pid, SIGUSR2);
-    } else {
-      kill(receiver_pid, SIGUSR1);
-    }
-    mask <<= 1;
-    for (i = 0; i < 30000000; ++i);
+  if (scanf("%d", &receiver_pid) < 0) {
+	printf("An error occurred while reading receiver PID.\n");
+	exit(-1);
   }
+	
+  printf("Enter a message that is an integer : ");
+  if (scanf("%d", &value) < 0) {
+	printf("An error occurred while reading the number.\n");
+	exit(-2);
+  }
+
+  send_value();
 
   printf("Sender has successfully completed its task\n");
   return 0;
